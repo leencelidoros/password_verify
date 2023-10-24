c8dba33748d291bc851b2ddeb4a704045666e0ee
Leence Lidoros <leencelidonde00@gmail.com>
2023-10-24 08:28:36 +0300

    Password Verification

--- pass.php
+++ pass.php
@@ -2,22 +2,22 @@
 // Hardcoded password
 $RealPassword = "MySuperSecretPassword";
 
-// Check if a password was provided as a command-line argument
+// Check if a password was provided as viA cmd 
 if ($argc < 2) {
     echo "Usage: php pass.php <password>\n";
     exit(1);
 }
 
-// Get the password from the command-line argument
+// Get the password from the CMD argument
 $password = $argv[1];
 
-// Hash the hardcoded password
+// Hash the real password
 $hashedPassword = password_hash($RealPassword, PASSWORD_DEFAULT);
 
 // Display the hashed password
 echo "Hashed Password: $hashedPassword\n";
 
-// Verify the provided password against the hashed version
+// Verify the entered password against the hashed one
 if (password_verify($password, $hashedPassword)) {
     echo "Password is correct!: $RealPassword\n";
 } else {
@@ -29,6 +29,6 @@ $wrongPassword = "WrongPassword";
 if (password_verify($wrongPassword, $hashedPassword)) {
     echo "Wrong password is correct, which shouldn't happen!\n";
 } else {
-    echo "Wrong password is indeed incorrect.\n";
+    echo "Oops !Wrong password is wrong.\n";
 }
 ?>
